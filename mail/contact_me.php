bbd80105bf292a86b8ea9370bf025b0adec42ac3
Nicole Brower <seow.nicole86@gmail.com>
2016-07-25 14:35:40 -0400

    hopefully my email works now

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,28 +1,45 @@
 <?php
-error_reporting(-1);
+require 'vendor/autoload.php';
+$sendgrid = new SendGrid('app52809917@heroku.com', 'Michelle86');
 
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'brower.nicole86@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+$email = new SendGrid\Email();
+$email->addTo('brower.nicole86@gmail.com')
+    ->setFrom('$email')
+    ->setSubject('Website Contact Form: $name')
+    ->setText($_POST['message'])
+    ->setHtml('You have received a new message from your website contact form.\n\n.Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message');
+
+$sendgrid->send($email);
+return true;
+// error_reporting(-1);
+//
+// // Check for empty fields
+// if(empty($_POST['name'])  		||
+//    empty($_POST['email']) 		||
+//    empty($_POST['phone']) 		||
+//    empty($_POST['message'])	||
+//    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+//    {
+// 	echo "No arguments Provided!";
+// 	return false;
+//    }
+//
+// $name = $_POST['name'];
+// $email_address = $_POST['email'];
+// $phone = $_POST['phone'];
+// $message = $_POST['message'];
+//
+// // Create the email and send the message
+// $to = 'brower.nicole86@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";
+// mail($to,$email_subject,$email_body,$headers);
+// return true;
 ?>
